87d1e6297b9f3dc67018437277b302b8f6ac7a59
dev <dev@Anhs-Mac-mini.local>
2018-11-19 12:14:22 +0700

    bvn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
 
    render() {
       return (
+
+
          <div className="product">
-            <img alt='' src="group-5.png" />
+
+            <div className="all">
+               <div className="screen">
+                  <div className="left">
+                     <div className="top" ><img alt='' src="group-5@3x.png" /></div>
+                     <div className="bot">
+                        <div className="content">Unsupported Browser on Mobile</div>
+                        <div className="text">We do not support OpenMarkets Equix on Mobile browser at the moment. Please download our OpenMarkets Equix Mobile App on  Google play or App store</div>
+                        <div className="content">Mobile App Now Available</div>
+                        <div className="lsbutton">
+                           <a href="#"><button className="button"><img alt='' src="1@3x.png"></img></button></a>
+                           <a href="#"><button className="button"><img alt='' src="2@3x.png"></img></button></a>
+                        </div>
+                     </div>
+                  </div>
+                  <div className="right">
+                     <div className="anh"><img alt='' src="group-6@3x.png"></img></div>
+                  </div>
+
+               </div>
+               <div className="triangle-bottomright"></div>
+            </div>
+
 
          </div>
+
       );
    }
 }
